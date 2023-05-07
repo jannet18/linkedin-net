f02a8c3d94721c5d5cc1e2ec80239cc7718a9387
Janet Mutindi <janetngei18@gmail.com>
2023-05-07 14:18:00 +0300

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,19 +15,43 @@ const Header = () => {
                 <input type="text" placeholder='Search' />
             </div>
             <SearchIcon>
-            <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 16 16"
-              data-supported-dps="16x16"
-              fill="currentColor"
-             width="16"
-              height="16"
-              focusable="false"
-            >
-              <path d="M14.56 12.44L11.3 9.18a5.51 5.51 0 10-2.12 2.12l3.26 3.26a1.5 1.5 0 102.12-2.12zM3 6.5A3.5 3.5 0 116.5 10 3.5 3.5 0 013 6.5z"></path>
-  </svg>
+                <img src="/images/search-icon.svg" alt="" />
             </SearchIcon>
         </Search>
+        <Nav>
+            <NavListWrap>
+                <NavList className='active'>
+                    <a>
+                        <img src="/images/nav-home.svg" alt="" />
+                        <span>Home</span>
+                    </a>
+                </NavList>
+                <NavList>
+                    <a>
+                        <img src="/images/nav-network.svg" alt="" />
+                        <span>My Network</span>
+                    </a>
+                </NavList>
+                <NavList>
+                    <a>
+                        <img src="/images/nav-jobs.svg" alt="" />
+                        <span>Jobs</span>
+                    </a>
+                </NavList>
+                <NavList>
+                    <a>
+                        <img src="/images/nav-messaging.svg" alt="" />
+                        <span>Messaging</span>
+                    </a>
+                </NavList>
+                <NavList>
+                    <a>
+                        <img src="/images/nav-notifications.svg" alt="" />
+                        <span>Notifications</span>
+                    </a>
+                </NavList>
+            </NavListWrap>
+        </Nav>
     </Content>
 </Container>
   )
@@ -40,7 +64,7 @@ left: 0;
 padding: 0 24px;
 postion: fixed;
 top: 0;
-width: 100vw;
+// width: 100%;
 z-index: 100;
 `;
 
@@ -92,4 +116,70 @@ display: flex;
 justify-content: center;
 align-items: center;
 `;
+
+const Nav = styled.nav`
+margin-left: auto;
+display: block;
+@media (max-width: 768px) {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    background: white;
+    width: 100%;
+}
+`;
+
+const NavListWrap = styled.ul`
+display: flex;
+flex-wrap: nowrap;
+list-style-type: none;
+.active {
+    span:after {
+        content: "";
+        transform: scaleX(1);
+        border-bottom: 2px solid var(--white, #fff);
+        bottom: 0;
+        left: 0;
+        position: absolute;
+        transition: transform 0.2s ease-in-out;
+        width: 100%;
+        border-color: rgba(0, 0, 0, 0.9);
+    }
+}
+  
+`;
+const NavList = styled.li`
+display: flex;
+align-items: center;
+a {
+    align-items: center;
+    background: transparent;
+    display: flex;
+    flex-direction: column;
+    font-size: 12px;
+    font-weight: 400;
+    justify-content: center;
+    line-height: 1.5;
+    min-height: 52px;
+    min-width: 80px;
+    position: relative;
+    text-decoration: none; 
+    span{
+        color: rgba(0, 0, 0, 0.6);
+        display: flex;
+        align-items: center;
+    }
+    @media (max-width: 768px) {
+        min-width: 70px;
+    }
+&:hover, &:active {
+    a{
+        span{
+            color: rgba(0, 0, 0, 0.9);
+        }
+    }
+}
+`;
+
+
 export default Header
